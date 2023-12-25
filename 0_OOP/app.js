2b9811ae3be1c512a7f90a5410dd49b8873f729a
David Nguyen <42897656+ibdavid7@users.noreply.github.com>
2023-12-25 22:25:48 +0000

    initial commit

---
+++ 0_OOP/app.js
@@ -0,0 +1,11 @@
+const ringo = {
+    name: 'Ringo',
+    band: 'Beatles',
+    greet: function(greeting) {
+        return `${this.name} says ${greeting}`
+    }
+}
+
+const greeting = 'yippie kay yeah motherfucker';
+
+const nums = [1,4,88,3,4,46];
